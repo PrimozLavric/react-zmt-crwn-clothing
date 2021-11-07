303d999cd4525ead18e2fe3a40278fbd33777b17
plavric <primoz.lavric@cosylab.com>
2021-11-07 12:28:21 +0100

    Updated shop data, and added addToCart functionalty.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -31,9 +31,7 @@ const Header = ({ currentUser, hidden }) => (
       }
       <CartIcon />
     </div>
-    {
-      hidden ? null : <CartDropdown />
-    }
+    {hidden ? null : <CartDropdown />}
   </div>
 )
 
